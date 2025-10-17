8208235c56442c80b35c818b9dc8a12bcb0f011c
Aman Baluni <your_email@example.com>
2025-10-17 21:31:10 +0530

    Multi-Role-Authentication Backend

--- Backend/src/middleware/isVendor.js
+++ Backend/src/middleware/isVendor.js
@@ -1,17 +1,26 @@
-const Vendor = require("../model/Vendor")
+const jwt = require("jsonwebtoken");
+const Vendor = require("../model/Vendor");
 
- const isVendor = async(req,res,next) =>{
-    try{
-        const vendor = await Vendor.findOne({user:req.user.id});
-        if(!vendor){
-            return res.status(403).json({ message: "Access denied. Vendor only." });
-        }
 
-        req.vendor = vendor;
-        next();
-    }catch(error){
-        console.error("vendor middleware error :", error);
-        res.status(500).json({ message: "Server error verifying vendor"});
+const vendorProtect = async (req, res, next) => {
+  try {
+    const authHeader = req.headers.authorization;
+    if (!authHeader || !authHeader.startsWith("Bearer ")) {
+      return res.status(401).json({ message: "Not authorized" });
     }
-}
-module.exports = isVendor;
+
+    const token = authHeader.split(" ")[1];
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+    const vendor = await Vendor.findById(decoded.id);
+    if (!vendor) return res.status(404).json({ message: "Vendor not found" });
+
+    req.vendor = vendor;
+    next();
+  } catch (error) {
+    console.error(error);
+    res.status(401).json({ message: "Invalid token" });
+  }
+};
+
+module.exports = vendorProtect;
--- Backend/src/model/Vendor.js
+++ Backend/src/model/Vendor.js
@@ -1,10 +1,31 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
 const vendorSchema = new mongoose.Schema({
-    user:{type:mongoose.Schema.Types.ObjectId,ref:"User",required:true},
+   name:{type:String,required:true},
+   email:{type:String,required:true,unique:true},
+   password:{type:String,required:true},
     shopName:{type:String,required:true},
     description:{type:String},
     verified:{type:Boolean,default:false}
 },{timestamps:true});
+
+vendorSchema.pre("save", async function(next){
+    if(!this.isModified("password")) return next();
+    this.password = await bcrypt.hash(this.password,10);
+    next();
+})
+
+vendorSchema.methods.getSignedToken = function(){
+    return jwt.sign({id: this._id,role:"vendor"},process.env.JWT_SECRET,{
+        expiresIn: "7d",
+    });
+};
+
+vendorSchema.methods.matchPassword = async function(enteredPassword){
+    return await  bcrypt.compare(enteredPassword,this.password);
+
+};
 
 module.exports = mongoose.model("Vendor",vendorSchema);
